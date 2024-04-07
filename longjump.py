bc3f9907cc3e5b8ef8fa9da6411c6223a31ce5bc
mariannehj <131137152+mariannehj@users.noreply.github.com>
2024-04-07 20:54:38 +0200

    Add files via upload

--- longjump.py
+++ longjump.py
@@ -1,23 +1,16 @@
 from constants import *
 from main import *
 
-
-class JumpingRopeGame(MiniGame):
+class LongJumpGame(MiniGame):
     def __init__(self):
         super().__init__()
-        self.player = PlayerRope(WIDTH // 2 - 50, HEIGHT - 220, 80, 110, RED)
-        self.rope = Rope(WIDTH//2, HEIGHT//2, 10, BLUE)
-        self.running = True  # Definer running-variabelen her
-        self.jump_over_rope = False
+        self.player = PlayerLong()
+        self.sand = Lane(BEIGE, SAND_X, laneR_Y, SAND_WIDTH, laneR_HEIGHT)
+        self.plank_b = Lane(BLACK, plank_B_X, laneR_Y, plank_B_WIDTH, laneR_HEIGHT)  
+        self.plank_w = Lane(WHITE, plank_W_X, laneR_Y, plank_W_WIDTH, laneR_HEIGHT)
+        self.start = Lane(RED, START_X, laneR_Y, START_WIDTH, laneR_HEIGHT)
+        self.lane = Lane(GREY, LOOPElane_X, laneR_Y, LOOPElane_WIDTH, laneR_HEIGHT)
         self.countdown_timer = 3
-
-    def redraw_window(self, player, rope):
-        screen.blit(self.background_img, (0, 0))
-        screen.blit(player_img, (self.player.x, self.player.y))  # Tegn playeren som seahorse.png
-        pg.draw.circle(screen, self.rope.color, (self.rope.x, self.rope.y), self.rope.radius)
-        score_text = font.render(f"Poengsum: {self.score}", True, RED)
-        screen.blit(score_text, (10, 90))
-        self.home_button.draw()
 
     def run(self):
         run = True
@@ -32,82 +25,123 @@ class JumpingRopeGame(MiniGame):
             pg.time.delay(1000) # Vent i 1 sekund
             self.countdown_timer -= 1
         
-        while self.running:
+        while run:
+            screen.blit(self.background_img, (0, 0))
             self.clock.tick(FPS)
+            self.home_button.draw()
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    self.running = False  
-            keys = pg.key.get_pressed()
-            if keys[pg.K_LEFT] and self.player.x > self.player.vel:
-                self.player.x -= self.player.vel
-            if keys[pg.K_RIGHT] and self.player.x < WIDTH - self.player.width - self.player.vel:
-                self.player.x += self.player.vel
-            # jump hvis mellomromstasten trykkes og playeren ikke allerede jumper
-            if not self.player.isJumping:
-                if keys[pg.K_SPACE]:
-                    self.player.isJumping = True
-            else:
-                # Utfør jump
-                if self.player.speed >= -10:
-                    neg = 1
-                    if self.player.speed < 0:
-                        neg = -1
-                    self.player.y -= (self.player.speed ** 2) * 0.5 * neg
-                    self.player.speed -= 1
-                else:
-                    self.player.isJumping = False
-                    self.player.speed = 10
+                    run = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_SPACE:
+                        if not self.player.running:
+                            self.player.start_movement()
+                            self.player.jump()
 
-            self.rope.update()  # Oppdater tauets posisjon
+        
+            self.lane.move(self.sand, self.player)
+            self.lane.draw()
+            screen.blit(self.lane_img, (LOOPElane_X, laneR_Y))
+            self.start.move(self.sand, self.player)
+            self.start.draw()
+            self.plank_b.move(self.sand, self.player)
+            self.plank_b.draw()
+            self.plank_w.move(self.sand, self.player)
+            self.plank_w.draw()            
+            self.sand.move(self.sand, self.player)
+            self.sand.draw()
+            self.player.move()
+            self.player.draw()
 
-            # Sjekk om playeren jumper over tauet og legg til poeng
-            if self.player.y < self.rope.y and self.rope.x - self.rope.radius < self.player.x < self.rope.x + self.rope.radius * 2 and not self.jump_over_rope:
-                self.score += 1
-                self.jump_over_rope = True
-            elif self.player.y >= self.rope.y:
-                self.jump_over_rope = False
-
-            # Sjekk om playeren og tauet kolliderer og avslutt spillet
-            self.player_rect = pg.Rect(self.player.x, self.player.y, self.player.width, self.player.height)
-            self.rope_rect = pg.Rect(self.rope.x - self.rope.radius, self.rope.y - self.rope.radius, 2 * self.rope.radius, 2 * self.rope.radius)
-            if self.player_rect.colliderect(self.rope_rect):
-                return self.score
+            
+            if self.player.rect.colliderect(self.sand.rect):
+                run = False
+                self.player.show_meters()
+                pg.display.flip()
+                pg.time.delay(3000)
                 
-            self.redraw_window(self.player, self.rope)  # Tegn spillet
+            elif self.player.rect.colliderect(self.plank_w.rect):
+                if not self.player.has_jumped:
+                    run = False
+                    self.player.dead()
+                    pg.display.flip()
+                    pg.time.delay(3000)
+                    
+            # Oppdater poengsummen med antall meter hoppet
+            self.score = self.player.meter * 10
 
             pg.display.flip()
-
-        return self.score  # Returner self.score når løkken er ferdig
-
-class PlayerRope:
-    def __init__(self, x, y, width, height, color):
+            
+        return int(self.score)
+        pg.quit()
+        sys.exit()
+        
+class Lane:
+    def __init__(self, color, x, y, w, h):
+        self.color = color
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.vel = 5
-        self.isJumping = False
-        self.speed = 10
+        self.w = w
+        self.h = h
+        self.fart = 0
+        self.aks = -0.12
+        self.rect = pg.Rect(self.x, self.y, self.w, self.h)
+    
+    def move(self, sand, player):
+        if not pg.Rect.colliderect(sand.rect, player.rect):
+            self.fart += self.aks
+            self.x += self.fart
+            self.rect.x = self.x
+        
+    def draw(self):
+        pg.draw.rect(screen, self.color, self.rect)
+ 
+class PlayerLong:
+    def __init__(self):
+        self.y_pos = 505  # Startposisjon
+        self.y_gravity = 1
+        self.jump_h = 20
+        self.y_speed = 0  
+        self.rect = pg.Rect(10, self.y_pos, 30, 50)  # Rektangelstørrelse
+        self.running = False  
+        self.meter = 0
+        self.sand = Lane(BEIGE, SAND_X, laneR_Y, SAND_WIDTH, laneR_HEIGHT)
+        self.score=0
+        self.has_jumped = False  # Legg til has_jumped attributt og sett til False
+        self.player_img = pg.image.load('seahorse.png')  # Last inn spillerens bilde
+        self.player_img = pg.transform.scale(self.player_img, (45, 55))  # Juster størrelsen
 
-class Rope:
-    def __init__(self, x, y, radius, color):
-        self.x = x
-        self.y = y
-        self.radius = radius  # Tauets radius
-        self.color = color
-        self.angle = -math.pi/1  # Startvinkel for tauet (endret til negativ verdi)
-        self.angular_speed = 0.08  # Hastighet for svinging av tauet
-        self.radius_multiplier = 1.15  # Multiplikator for å endre tauets lengde
+    def start_movement(self):
+        self.running = True
 
-    def update(self):
-        self.angle += self.angular_speed  # Oppdater vinkelen til tauet
-        self.angular_speed += 0.00005  # Øk svinghastigheten litt for hver oppdatering
-        # Beregn nye koordinater for tauet basert på vinkelen og multiplikatoren
-        self.x = int(WIDTH // 2 + math.cos(self.angle) * (WIDTH // 4) * self.radius_multiplier)
-        self.y = int(HEIGHT // 2 + math.sin(self.angle) * (HEIGHT // 4) * self.radius_multiplier)
+    def jump(self):
+        if self.running and self.y_pos == 505:
+            if not self.has_jumped:
+                self.has_jumped = True
+                self.y_speed = -self.jump_h
+            
+    def move(self):
+        if self.y_pos < 505:
+            self.y_speed += self.y_gravity
+        self.y_pos += self.y_speed
+
+        if self.y_pos >= 505:
+            self.y_pos = 505
+            self.y_speed = 0
+            self.meter = abs(505-self.sand.rect.x) / 100
+        self.rect.y = self.y_pos - self.player_img.get_height()
 
     def draw(self):
-        # Tegn tauet som en sirkel
-        pg.draw.circle(self.color, (self.x, self.y), self.radius)
+        self.rect.y = self.y_pos
+        screen.blit(self.player_img, self.rect) 
+    
+    def show_meters(self):
+        text_img = font.render(f'Du har hoppet {self.meter:.1f} meter.', True, BLUE)
+        text_rect = text_img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(text_img, text_rect)
         
+    def dead(self):
+        text_img = font.render(f'Hoppet ble dessverre dødt', True, BLUE)
+        text_rect = text_img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(text_img, t
